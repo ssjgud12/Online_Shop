d6eeb6a7453f7fb1063e642b39c28a09dcddd863
Alvin Olayemi <g00428825@atu.ie>
2025-10-19 21:11:48 +0100

    Fixed mixed up product/productcontroller

--- src/test/java/ie/atu/onlineshopp/Product.java
+++ src/test/java/ie/atu/onlineshopp/Product.java
@@ -1,4 +1,14 @@
 package ie.atu.onlineshopp;
 
-public class Product {
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Positive;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+
+public class Product
+{
+    @NotBlank
+    private String ProductName;
+    @Positive
+    private double Price;
 }
--- src/test/java/ie/atu/onlineshopp/ProductController.java
+++ src/test/java/ie/atu/onlineshopp/ProductController.java
@@ -1,17 +1,36 @@
 package ie.atu.onlineshopp;
 
+import io.swagger.v3.oas.annotations.parameters.RequestBody;
+import jakarta.validation.Valid;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.Positive;
-import lombok.AllArgsConstructor;
-import lombok.Data;
+import java.util.ArrayList;
+import java.util.List;
 
 
-public class ProductController
-{
-    @NotBlank
-    private String ProductName;
-    @Positive
-    private double Price;
+@RequestMapping("/product")
+@RestController
+public class ProductController {
+    List<Product> myList = new ArrayList<>();
+    @GetMapping("/hello")
+    public String hello()
+    {
+        return "Good Morning World!";
+    }
 
+    @GetMapping("/getproducts")
+    public List<Product> getProducts()
+    {
+        Product myProduct = new Product("Tv",677);
+        return myList;
+    }
+    @PostMapping("/addProduct")
+    public Product addProduct(@Valid @ie.atu.onlineshopp.RequestBody Product myproduct)
+    {
+        myList.add(myproduct);
+        return myproduct;
+    }
 }
