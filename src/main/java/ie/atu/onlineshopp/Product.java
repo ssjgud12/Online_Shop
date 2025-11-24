99708e86f0ceddd3f039e7a610286234cd521e8d
Alvin Olayemi <g00428825@atu.ie>
2025-11-24 22:53:17 +0000

    filled Product class

--- src/main/java/ie/atu/onlineshopp/Product.java
+++ src/main/java/ie/atu/onlineshopp/Product.java
@@ -1,5 +1,19 @@
 package ie.atu.onlineshopp;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.Size;
+import lombok.AllArgsConstructor;
+
+@AllArgsConstructor
 public class Product
 {
+
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    @Size(max = 60)
+    private String email;
 }
