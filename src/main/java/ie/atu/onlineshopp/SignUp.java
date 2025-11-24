fcc57712fdd78528ea749c8875b37595866d3a91
Alvin Olayemi <g00428825@atu.ie>
2025-11-24 23:29:08 +0000

    Created Sign up

--- src/main/java/ie/atu/onlineshopp/SignUp.java
+++ src/main/java/ie/atu/onlineshopp/SignUp.java
@@ -6,14 +6,14 @@ import jakarta.validation.constraints.Size;
 import lombok.AllArgsConstructor;
 
 @AllArgsConstructor
-public class Product
+public class SignUp
 {
 
     @NotBlank
-    private String name;
+    private String Password;
 
     @NotBlank
     @Email
     @Size(max = 60)
-    private String email;
+    private String Email;
 }
---
+++ src/main/java/ie/atu/onlineshopp/SignUpController.java
@@ -0,0 +1,19 @@
+package ie.atu.onlineshopp;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/sign-up")
+public class SignUpController {
+
+    @GetMapping
+    public String SignUp ( @RequestParam @Email String email,  @RequestParam @NotBlank String password)
+    {
+        return "Customer signed up with Email: " + email + " and Password: " + password;
+    }
+}
