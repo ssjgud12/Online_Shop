61337c2df06ac62f206bd100b10ca325af13eae0
Alvin Olayemi <g00428825@atu.ie>
2025-11-29 21:04:17 +0000

    Added @Service to ProductService

--- src/main/java/ie/atu/onlineshopp/controller/ProductController.java
+++ src/main/java/ie/atu/onlineshopp/controller/ProductController.java
@@ -1,15 +1,29 @@
 package ie.atu.onlineshopp.controller;
 
+import ie.atu.onlineshopp.model.Product;
 import ie.atu.onlineshopp.service.ProductService;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("api/Products")
 public class ProductController
 {
+  private ProductService productService;
 
+  public ProductController (ProductService productService)
+  {
+      this.productService = productService;
+  }
 
-
+  @GetMapping
+    public ResponseEntity<List<Product>> getAll()
+  {
+      return ResponseEntity.ok(service.findAll());
+  }
 
 }
--- src/main/java/ie/atu/onlineshopp/service/ProductService.java
+++ src/main/java/ie/atu/onlineshopp/service/ProductService.java
@@ -1,11 +1,13 @@
 package ie.atu.onlineshopp.service;
 
 import ie.atu.onlineshopp.model.Product;
+import org.springframework.stereotype.Service;
 
 import java.util.ArrayList;
 import java.util.List;
 import java.util.Optional;
 
+@Service
 public class ProductService
 {
 
