2330ab97d71060f4a4c6627fd53bd782eb1959b4
Alvin Olayemi <g00428825@atu.ie>
2025-11-30 16:05:38 +0000

    Added endpoint to change price,quantity

--- src/main/java/ie/atu/onlineshopp/model/Product.java
+++ src/main/java/ie/atu/onlineshopp/model/Product.java
@@ -29,5 +29,62 @@ public class Product
     {
         this.id = id;
     }
+    public void setQuantity(int quantity)
+    {
+        this.quantity = quantity;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public int getQuantity() {
+        return quantity;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public String getManufacturer() {
+        return manufacturer;
+    }
+
+    public void setManufacturer(String manufacturer) {
+        this.manufacturer = manufacturer;
+    }
+
+    public void setPrice(double price)
+    {
+        this.price = price;
+    }
 
 }
