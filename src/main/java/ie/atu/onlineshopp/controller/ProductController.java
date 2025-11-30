207e642a263a3499012a62fefc093a5dc61b9f3d
Alvin Olayemi <g00428825@atu.ie>
2025-11-30 14:44:06 +0000

    Http requests JSON return responses

--- src/main/java/ie/atu/onlineshopp/controller/ProductController.java
+++ src/main/java/ie/atu/onlineshopp/controller/ProductController.java
@@ -4,16 +4,18 @@ import ie.atu.onlineshopp.model.Product;
 import ie.atu.onlineshopp.service.ProductService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.List;
+import java.util.Optional;
 
 @RestController
 @RequestMapping("api/Products")
 public class ProductController
 {
-  private ProductService productService;
+  private final ProductService productService;
 
   public ProductController (ProductService productService)
   {
@@ -23,7 +25,22 @@ public class ProductController
   @GetMapping
     public ResponseEntity<List<Product>> getAll()
   {
-      return ResponseEntity.ok(service.findAll());
+      return ResponseEntity.ok(productService.findAll());
   }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getOne(@PathVariable int id)
+    {
+
+        Optional<Product> maybe = productService.findById(id);
+        if(maybe.isPresent())
+        {
+            return ResponseEntity.ok(maybe.get());
+        }
+        else
+        {
+            return ResponseEntity.notFound().build();
+        }
+    }
 
 }
