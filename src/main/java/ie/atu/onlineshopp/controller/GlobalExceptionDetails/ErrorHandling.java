db4569e368d9ec27f1bc3aa7eb7367d8f80bf0f2
Alvin Olayemi <g00428825@atu.ie>
2025-12-03 23:47:57 +0000

    Added Basic error message

--- src/main/java/ie/atu/onlineshopp/controller/GlobalExceptionDetails/ErrorHandling.java
+++ src/main/java/ie/atu/onlineshopp/controller/GlobalExceptionDetails/ErrorHandling.java
@@ -2,5 +2,9 @@ package ie.atu.onlineshopp.controller.GlobalExceptionDetails;
 
 public class ErrorHandling
 {
+    public String ShowErrorDetails()
+    {
+        return "There is an issue";
+    }
 
 }
