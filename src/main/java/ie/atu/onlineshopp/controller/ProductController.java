9677092b33a3c8adcf688221a0e7d0a087f215ab
Alvin Olayemi <g00428825@atu.ie>
2026-01-09 18:33:53 +0000

    Added real all Products

--- src/main/java/ie/atu/onlineshopp/controller/ProductController.java
+++ src/main/java/ie/atu/onlineshopp/controller/ProductController.java
@@ -2,81 +2,34 @@ package ie.atu.onlineshopp.controller;
 
 import ie.atu.onlineshopp.model.Product;
 import ie.atu.onlineshopp.service.ProductService;
-import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import org.springframework.http.ResponseEntity;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
-import java.awt.*;
-import java.net.URI;
-import java.util.ArrayList;
 import java.util.List;
-import java.util.Optional;
 
 @RestController
-@RequestMapping("/Products")
-@Tag(name = "Sign Up Controller", description = "APIs for managing user sign ups")
-public class ProductController
-{
-    List<Product> myList = new ArrayList<>();
-//Bean created by Spring
+@RequestMapping("/api/products")
+public class ProductController {
 
-    private final ProductService productService;
-    //Used to access product logic
-    public ProductController (ProductService productService)
-    {
-        this.productService = productService;
-    }
-    //Returns all products in the store
-    @GetMapping("/display")
-    public ResponseEntity<List<Product>> getAll()
-    {
-        return ResponseEntity.ok(productService.findAll());
+    private final ProductService service;
+
+    public ProductController(ProductService service) {
+        this.service = service;
     }
 
-
-    //If a product with the Given id exists return it, If not return 404 Not found
-    @GetMapping("/{id}")
-    public ResponseEntity<Product> getOne(@PathVariable int id)
+    // CREATE - POST
+    @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
+    public Product create(@Valid @RequestBody Product product)
     {
-
-        Optional<Product> maybe = productService.findById(id);
-        if(maybe.isPresent())
-        {
-            return ResponseEntity.ok(maybe.get());
-        }
-        else
-        {
-            return ResponseEntity.notFound().build();
-        }
-    }
-    // This endpoint creates a new product evertthing except price and quantity are dummy values for now
-
-    @PostMapping("Add")
-    public ResponseEntity<Product> create(@Valid @RequestBody Product p)
-    {
-        Product newProduct = productService.create(p);
-        return ResponseEntity.created(URI.create("/api/Products/" + newProduct.getId())).body(newProduct);
+        return service.create(product);
     }
 
-    @PutMapping("/Update{id}")
-    public ResponseEntity<Product> update(@PathVariable int id, @RequestBody Product product)
-    {
-
-        Optional<Product> updated = productService.update(id,product);
-        return updated.map(ResponseEntity::ok)
-                .orElse(ResponseEntity.notFound().build());
+    // READ ALL - GET
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return service.getAllProducts();
     }
-
-    @DeleteMapping("/Delete{id}")
-    public ResponseEntity<Void> delete(@PathVariable int id)
-    {
-        boolean deleted = productService.delete(id);
-        return deleted ? ResponseEntity.noContent().build()
-                : ResponseEntity.notFound().build();
-    }
-
-
-
 
 }
--- src/main/java/ie/atu/onlineshopp/service/ProductService.java
+++ src/main/java/ie/atu/onlineshopp/service/ProductService.java
@@ -15,72 +15,6 @@ import java.util.concurrent.atomic.AtomicInteger;
 public class ProductService
 {
 
-    private final List<Product> store = new ArrayList<>();
-
-    public List<Product> findAll() {
-        return new ArrayList<>(store);
-    }
-
-    public Optional<Product> findById(int id)
-    {
-        for (Product product : store)
-        {
-            if (product.getId() == id)
-            {
-                return Optional.of(product);
-            }
-        }
-        return Optional.empty();
-    }
-
-
-    private AtomicInteger counter = new AtomicInteger(1);
-
-
-
-    public Optional<Product> update(int id, Product updatedProduct)
-    {
-        for (int i = 0; i < store.size(); i++)
-        {
-            Product existing = store.get(i);
-            if (existing.getId() == id)
-            {
-                existing.setName(updatedProduct.getName());
-                existing.setDescription(updatedProduct.getDescription());
-                existing.setPrice(updatedProduct.getPrice());
-                existing.setQuantity(updatedProduct.getQuantity());
-                existing.setCategory(updatedProduct.getCategory());
-                existing.setImage(updatedProduct.getImage());
-                existing.setManufacturer(updatedProduct.getManufacturer());
-
-
-                store.set(i, existing);
-
-                return Optional.of(existing);
-            }
-        }
-
-        return Optional.empty();
-    }
-
-
-    public boolean delete(int id)
-    {
-        for (int i = 0; i < store.size(); i++)
-        {
-            Product p = store.get(i);
-
-
-            if (p.getId() == id)
-            {
-                store.remove(i);
-                return true;
-            }
-        }
-        return false;
-
-    }
-
     private final ProductRepository repo;
 
     public ProductService(ProductRepository repo)
@@ -91,6 +25,11 @@ public class ProductService
     public Product create(Product p)
     {
         return repo.save(p);
+    }
+
+    public List<Product> getAllProducts()
+    {
+        return repo.findAll();
     }
 
 
